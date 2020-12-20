9e7ffe877134fe3e6932ece8618afc3e334fa455
xumiSky <hmxing1105@163.com>
2020-12-21 02:43:51 +0800

    v.10版本

---
+++ README.md
@@ -0,0 +1,125 @@
+# laydateTable
+
+## 介绍
+基于layui的laydate实现的日期时间选择组件选项卡，可以支持多个日期类型切换选择，且对于laydate的配置全部支持。
+
+
+## 安装教程
+依赖laydate，所以必须引用laydate!  
+插件目录一定为以下结构：
+```javascript
+|-- extends  
+	|-- css
+		|-- laydateTab.css
+	|-- laydateTab.js
+```
+
+### 1. 使用layui模块化引入
+项目目录结构假设为：
+```javascript
+|-- project
+	|-- js
+		|-- layui
+			|-- css
+				|-- layui.css
+			|-- extends
+				|-- css
+					|-- laydateTab.css
+				|-- laydateTab.js
+			|-- lay
+				...
+			|-- font
+				...
+			|-- images
+				...
+			|-- layui.all.js
+			|-- layui.js
+```
+**则引用方式为：**  
+1.  先加载layui相关的js、css
+2.  模块加载代码如下  
+```javascript  
+layui.config({  
+		base: 'js/layui/extends/'
+}).extend({ 
+		laydateTab: 'laydateTab' 
+}).use(['laydateTab'], function(){
+		var laydateTab = layui.laydateTab;
+});
+```
+
+### 2. 使用laydate单独引入
+项目目录结构假设为：
+```javascript
+	|-- project
+		|-- js
+			|-- laydate
+				|-- theme
+					|-- default
+						|-- font
+							...
+						|-- laydate.css
+			|-- extends
+				|-- css
+					|-- laydateTab.css
+				|-- laydateTab.js
+```
+**则引用方式为：**  
+```javascript
+<script src="js/laydate/laydate.js" type="text/javascript" charset="utf-8"></script>
+<script src="js/layui/extends/laydateTab.js" type="text/javascript" charset="utf-8"></script>
+```
+### 使用说明
+#### 获取laydateTab对象 
+1.  使用layui模块化引入，使用``layui.laydateTab``获取laydateTab对象。  
+```javascript
+layui.config({
+		base: 'js/layui/extends/' //假设这是你存放拓展模块的根目录
+}).extend({ //设定laydateTab模块别名
+		laydateTab: 'laydateTab' 
+}).use(['laydateTab'], function(){
+		var laydateTab = layui.laydateTab;
+		laydateTab.render({
+				elem : '#indate'
+		});
+});
+```
+2.  使用laydate模块单独引入，使用``laydateTab``获取laydateTab对象。  
+```javascript
+laydateTab.render({  
+		elem : '#indate'
+});
+```
+
+3.  具体例子参考 **examples** 文件夹里面。
+
+#### laydateTab特有的相关配置
+
+|参数	|类型	|说明	|示例值		|
+---|---|---|---
+elem | String/DOM | 指定laydateTab的触发的元素，必填 | '#demo'
+trigger | String | 触发显示laydateTab的事件，默认值：click | 'mouseover'
+type | Array / String | 需要渲染的选项卡类型。<br>1.  默认值: ['year', 'month', 'date'] <br> 2.   选项卡类型值：<br> year : 年 <br> month : 月 <br> date : 日期 <br> time : 时间 <br> datetime : 日期时间 <br>3.  内置String快捷类型：<br> yms : 年、月、日期3个选项卡 <br>ym : 年、月2个选项卡 <br>md : 月、日期2个选项卡<br>all : 年、月、日期、时间、日期时间全部5个选项卡 | 'yms' <br> [year', 'month', 'date']
+title |  Array | [选填]日期选择选项卡显示的标题名称，如果填写，则使用数组写出和type对应的选项卡标题名称，如果数组中某个值为null，则使用内置默认值。<br>默认值如下：<br>year: 年<br>month: 月<br>date: 日期<br>datetime: 日期时间<br>time: 时间 | ['年份', '月份', '日期']
+value |  Array / String | 日期默认值,使用数组则需要和type一一对应。 | '2020-12-01'
+selected |  String | 初始化默认显示的选项卡。 | 'date'
+theme | String | 主题；theme的可选值有：default（默认简约）、molv（墨绿背景）、#颜色值（自定义颜色背景）、grid（格子主题）| '#393D49'
+locationValueTab | Boolean | 有初始值情况下，是否定位到初始值对应的选项卡类型,默认true，false不进行定位 | false
+success | Function | 渲染显示成功之后的回调，返回2个参数，分别为日期选项卡对应的标签元素、渲染配置 | function(tabElem, options)  {<br> console.log(tabElem);<br>console.log(options);<br>}
+changeTab | Function | 切换选项卡的回调，返回1个参数-选项卡对应的类型、类型标题名称 | function(type, title)  {<br> console.log(type);<br> console.log(title);<br>}
+done | Array / Function | 选择完毕的回调,如果是数组，则数组存放的也需要为function，回调参数带有3个参数分别代表：生成的值、日期时间对象、结束的日期时间对象；如果return String类型的值回去，则将return的值给elem赋值<br> | function(value, date, endDate){<br>console.log(value);<br>console.log(date);<br>console.log(endDate);<br>}
+end | Function | 销毁选项卡之后的回调，返回1个参数-所绑定的elem对象 | function(elem)  {<br> console.log(elem);<br>}
+
+*tips : `position`参数不可使用*
+
+#### 其他相关配置
+
+其他配置参考[laydate配置](https://www.layui.com/doc/modules/laydate.html#onchange), 选项卡全通用则使用**单个值**，区分选项卡则使用**数组**。
+
+
+#### 参与贡献
+
+1.  Fork 本仓库
+2.  新建 Feat_xxx 分支
+3.  提交代码
+4.  新建 Pull Request  
---
+++ src/laydateTab.js
@@ -0,0 +1,440 @@
+/**
+ * laydateTab v1.0
+ * @Name：多个laydate选择框, 依赖laydate
+ * @Author：xumi
+ * MIT License
+ */
+;! function() {
+	"use strict";
+	var isLayui = window.layui && layui.define,
+	ready = {
+		getPath: function() {
+			var jsPath = document.currentScript ? document.currentScript.src : function() {
+				var js = document.scripts,
+					last = js.length - 1,
+					src;
+				for (var i = last; i > 0; i--) {
+					if (js[i].readyState === 'interactive') {
+						src = js[i].src;
+						break;
+					}
+				}
+				return src || js[last].src;
+			}();
+			return jsPath.substring(0, jsPath.lastIndexOf('/') + 1);
+		}(),
+		//获取节点的style属性值
+		getStyle: function(node, name) {
+			var style = node.currentStyle ? node.currentStyle : window.getComputedStyle(node, null);
+			return style[style.getPropertyValue ? 'getPropertyValue' : 'getAttribute'](name);
+		},
+		//载入CSS配件
+		link: function(href, fn, cssname) {
+			//未设置路径，则不主动加载css
+			if (!laydateTab.path) return;
+
+			var head = document.getElementsByTagName("head")[0],
+				link = document.createElement('link');
+			if (typeof fn === 'string') cssname = fn;
+			var app = (cssname || href).replace(/\.|\//g, '');
+			var id = 'layuicss-' + app,
+				timeout = 0;
+
+			link.rel = 'stylesheet';
+			link.href = laydateTab.path + href;
+			link.id = id;
+
+			if (!document.getElementById(id)) {
+				head.appendChild(link);
+			}
+
+			if (typeof fn !== 'function') return;
+
+			//轮询css是否加载完毕
+			(function poll() {
+				if (++timeout > 8 * 1000 / 100) {
+					return window.console && console.error(id + '.css: Invalid');
+				};
+				parseInt(ready.getStyle(document.getElementById(id), 'width')) === 1989 ? fn() : setTimeout(poll, 100);
+			}());
+		}
+	};
+
+	// 真正实现
+	var lay,
+	laydate,
+	laydateTab = {
+		v: '1.0',
+		config: {},
+		path: ready.getPath,
+		//主体CSS等待事件
+		ready: function(fn) {
+			lay = window.lay;
+			laydate = laydate ? laydate : (isLayui ? layui.laydate : window.laydate);
+			var cssname = 'laydateTab',
+			path = 'css/laydateTab.css?v=' + laydateTab.v;
+			ready.link(path, fn, cssname);
+			return this;
+		},
+		// 加载css
+		link: function(path, fn, cssname) {
+			ready.link(path, fn, cssname);
+		}
+	},
+	// 常量
+	MOD_NAME = 'laydateTab',
+	INDEX = 0,
+	MAIN_CLASS = 'laydate-tab',
+	TITLE_CLASS = 'laydate-tab-title',
+	PANL_CLASS = 'laydate-tab-panl',
+	SELECTED_CLASS = 'selected',
+	SELECTED_CSS = '.laydate-tab>ul>li.selected',
+	FORMAT_REG_EXP = 'yyyy|y|MM|M|dd|d|HH|H|mm|m|ss|s',
+	// 替换格式，避免提示错误
+	FOAMAT_REPLACE_REG = {
+		year: /(M{1,2}|d{1,2}|H{1,2}|m{1,2}|s{1,2})[^y]/g,
+		month: /(d{1,2}|H{1,2}|m{1,2}|s{1,2})[^M]/g,
+		date: /(H{1,2}|m{1,2}|s{1,2})[^d]/g,
+		time: /(y{2,4}|M{1,2}|d{1,2})[^H]/g
+	},
+	FORMAT = {
+		year: 'yyyy',
+		month: 'yyyy-MM',
+		date: 'yyyy-MM-dd',
+		time: 'HH:mm:ss',
+		datetime: 'yyyy-MM-dd HH:mm:ss'
+	},
+	// 快捷方式
+	TAB_FAST_TYPE = {
+		ymd: ['year', 'month', 'date'], // 默认
+		ym: ['year', 'month'],
+		md: ['month', 'date'],
+		all: ['year', 'month', 'date', 'time', 'datetime']
+	},
+	// 默认tab页名称
+	TAB_DEFUALT_TITLE = {
+		year: '年',
+		month: '月',
+		date: '日期',
+		datetime: '日期时间',
+		time: '时间'
+	},
+	// 值转换成数组
+	valueToArray = function(value) {
+		return value.constructor === Array ? value : [value];
+	},
+	// 从数组中获取值
+	getValueByArray = function(arrs, i) {
+		return typeof arrs[i] !== 'undefined' ? arrs[i] : arrs[0];
+	},
+	// 实例对象
+	Class = function(options) {
+		if (!laydate) {
+			console.error('没有找到layui中laydate组件，无法进行渲染！');
+			return;
+		}
+		if (!options.elem) {
+			console.error('没有绑定相关的触发元素！');
+			return;
+		};
+		this.config = this.getDefualtConfig();
+		// 覆盖相关的数组值
+		for(var key in this.config) {
+			if (options[key] && this.config[key].constructor === Array) {
+				delete this.config[key];
+			}
+		}
+		this.config = lay.extend({}, this.config, options);
+		// btns 特殊处理
+		
+		options.btns && (this.config.btns = typeof options.btns[0] === 'string' ? [options.btns] : options.btns);
+		
+		this.elem = lay(options.elem);
+		this._id = MOD_NAME + INDEX++;
+		// 定制皮肤#颜色值
+		if(/^#/.test(options.theme)){
+		  var style = lay.elem('style')
+		  ,styleText = '#' + this._id + SELECTED_CSS + '{background: '+ options.theme + '}';
+		  if('styleSheet' in style){
+			style.setAttribute('type', 'text/css');
+		    style.styleSheet.cssText = styleText;
+		  } else {
+		    style.innerHTML = styleText;
+		  }
+		  lay('body').append(style);
+		}
+		// 如果值为字符串则应该初始化值给elem
+		typeof options.value === 'string' && (this.val(options.value), delete this.config.value);
+		this.bindEvent();
+	};
+
+	// 默认配置
+	Class.prototype.getDefualtConfig = function(){
+		return {
+			trigger: 'click', // 触发显示的事件
+			type: TAB_FAST_TYPE.ymd // 默认显示年月日
+		};
+	}
+
+	// 渲染
+	Class.prototype.render = function() {
+		this.creteTab();
+	}
+
+	// 创建相关的元素
+	Class.prototype.creteTab = function() {
+		var options = this.config,
+		targetElem = this.elem,
+		tabElem = lay(lay.elem('div', {'class': MAIN_CLASS, id: this._id})),
+		titleElem = this.titleElem = lay(lay.elem('ul', {'class': TITLE_CLASS})),
+		panlElem = this.panlElem = lay(lay.elem('div', {'class': PANL_CLASS})),
+		// 解析配置
+		types = this.types = this.getTypes(options.type),
+		typeLen = types.length,
+		elemValue = this.isInput(targetElem[0]) ? targetElem[0].value : targetElem[0].innerHTML,
+		// 默认选择项
+		selectedType = options.selected ? options.selected : types[0],
+		selectedType = TAB_DEFUALT_TITLE[selectedType] ? selectedType : types[0],
+		selectIndex = null;
+		this.tabElem = tabElem;
+		delete options.type;
+		this.laydateObj = [];
+		this.laydateOptions = [];
+
+		for (var i = 0; i < typeLen; i++) {
+			var option = {},
+			type = option.type = types[i],
+			selectedClass = type === selectedType ? {'class' : SELECTED_CLASS} : null,
+			laytitleContainer = lay(lay.elem('li', selectedClass)),
+			laydateContainer = lay.elem('div', selectedClass);
+
+			laytitleContainer[0].index = i;
+			for (var key in options) {
+				var value = options[key],
+				value = valueToArray(value),
+				_thatValue = getValueByArray(value, i);
+				_thatValue && (option[key] = _thatValue);
+			}
+			laytitleContainer.html(option.title ? option.title : TAB_DEFUALT_TITLE[type]);
+			option.done = this.doneFn(option.done);
+			option.position = 'static';
+			option.elem = laydateContainer;
+			var regExp = FOAMAT_REPLACE_REG[type],
+			_value = elemValue || option.value,
+			format = option.format;
+			// 格式处理
+			option.format = (format ? (regExp ? format.replace(regExp, '') : format) : FORMAT[type]).replace(/\s$/, '');
+			// 初始化值
+			if (_value && this.isInitValue(option, _value)) {
+				option.value = _value;
+				// 是否定位到初始值的tab页【默认定位】
+				options.locationValueTab !== false && (selectIndex = i);
+			}
+			this.laydateObj.push(laydate.render(option));
+			titleElem.append(laytitleContainer[0]);
+			panlElem.append(laydateContainer);
+			this.changeEvent(option, laytitleContainer);
+		}
+		options.valueToSelect
+
+		tabElem.append(titleElem[0]);
+		tabElem.append(panlElem[0]);
+		lay('body').append(tabElem[0]);
+		// 绑定事件，阻止事件冒泡
+		tabElem.on('click', function(e) {
+			e && e.stopPropagation ? e.stopPropagation() : (e.cancelBubble = true);
+		});
+		// 定位到初始值的tab页
+		selectIndex !== null && this.changeTab(selectIndex);
+		this.position();
+	}
+
+	// 校验是否需要初始化值
+	Class.prototype.isInitValue = function(option, value){
+		var that = this,
+		formats = option.format.match(new RegExp(FORMAT_REG_EXP + '|.', 'g')) || [],
+		reg = '',
+		isInitValue = false;
+		// 拼接正则表达式
+		lay.each(formats, function(i, item){
+			var len = item.length;
+		    reg += ('(' + (/y|M|d|H|m|s/.test(item) ? '\\d{' + len +','+ (len > 2 ? 4 : 2) +'}' : item) + ')');
+		});
+		reg = new RegExp('^' + reg + '$');
+		// 范围
+		if(option.range){
+			var rangeStr = ' ' + option.range + ' ',
+			valueArr = value.split(rangeStr);
+			value = valueArr[0];
+			var endDate = valueArr[1];
+			if (!reg.test(endDate)) {
+				return false;
+			}
+		}
+		isInitValue = reg.test(value);
+		return isInitValue;
+	}
+
+	// 获取渲染的类型
+	Class.prototype.getTypes = function(type) {
+		if (this.types) {
+			return this.types;
+		}
+		var typeArr = typeof type === 'string' ? TAB_FAST_TYPE[type] || [type] : type,
+			exist = {},
+			types = [];
+		// 去重
+		for (var i = 0, len = typeArr.length; i < len; i++) {
+			!exist[typeArr[i]] && (exist[typeArr[i]] = true) && types.push(typeArr[i]);
+		}
+		return types;
+	}
+
+	// 完成之后函数
+	Class.prototype.doneFn = function(fn) {
+		var that = this;
+		return function(value, date, endDate) {
+			that.value = value;
+			if (typeof fn === 'function') {
+				that.value = fn(value, date, endDate) || value;
+			}
+			that.val(that.value);
+			that.remove();
+		}
+	}
+	Class.prototype.val = function(value){
+		var valFn = this.isInput(this.elem[0]) ? 'val' : 'html';
+		this.elem[valFn](value);
+	}
+
+	//是否输入框
+	Class.prototype.isInput = function(elem){
+	  return /input|textarea/.test(elem.tagName.toLocaleLowerCase());
+	};
+
+	// 标题栏切换事件
+	Class.prototype.changeEvent = function(option, titleContainer) {
+		var changeFn = option.changeTab,
+		type = option.type,
+		that = this;
+		// 绑定切换事件
+		titleContainer.on('click', function() {
+			if (!lay(this).hasClass(SELECTED_CLASS)) {
+				that.changeTab(this.index, changeFn, type);
+			}
+		});
+	}
+	Class.prototype.changeTab = function(index, changeFn, type){
+		var titleElem = this.titleElem[0],
+		panlElem = this.panlElem[0],
+		panlChilds = panlElem.childNodes,
+		titleChilds = titleElem.childNodes;
+		for (var i = 0, len = titleChilds.length; i < len; i++) {
+			var clsFn = i === index ? 'addClass' : 'removeClass'
+			lay(titleChilds[i])[clsFn](SELECTED_CLASS);
+			lay(panlChilds[i])[clsFn](SELECTED_CLASS);
+		}
+		typeof changeFn === 'function' && changeFn(type, titleChilds[index].innerHTML);
+	}
+
+	// 绑定事件
+	Class.prototype.bindEvent = function() {
+		var options = this.config,
+			elem = this.elem,
+			that = this;
+		// 显示
+		elem.on(options.trigger, function(elem, bind) {
+			if (that.tabElem) {
+				return;
+			}
+			that.render();
+		});
+
+		// 隐藏
+		lay(document).on('click', function(e) {
+			if (!that.tabElem || e.target === elem[0] || e.target === that.tabElem[0]) {
+				return;
+			}
+			that.remove();
+		});
+		// 自适应
+		lay(window).on('resize', function(e) {
+			that.remove(true);
+		});
+		lay(document).on('scroll', function(e) {
+			that.remove(true);
+		});
+	}
+
+	//定位算法
+	Class.prototype.position = function(){
+	  var that = this
+	  ,options = that.config
+	  ,elem = that.elem[0]
+	  ,tabElem = that.tabElem[0]
+	  ,rect = elem.getBoundingClientRect() //绑定元素的坐标
+	  ,elemWidth = tabElem.offsetWidth //控件的宽度
+	  ,elemHeight = tabElem.offsetHeight //控件的高度
+
+	  ,winArea = function(type){
+	    return document.documentElement[type ? 'clientWidth' : 'clientHeight']
+	  }, margin = 5, left = rect.left, top = rect.bottom;
+
+	  //如果右侧超出边界
+	  if(left + elemWidth + margin > winArea('width')){
+	    left = winArea('width') - elemWidth - margin;
+	  }
+
+	  //如果底部超出边界
+	  if(top + elemHeight + margin > winArea()){
+	    top = rect.top > elemHeight //顶部是否有足够区域显示完全
+	      ? rect.top - elemHeight
+	    : winArea() - elemHeight;
+	    top = top;
+	  }
+
+	  tabElem.style.left = left + 'px';
+	  tabElem.style.top = top + 'px';
+	  // 成功渲染之后的回调
+	  typeof options.success === 'function' && options.success(tabElem, options);
+	};
+
+	// 移除组件
+	Class.prototype.remove = function(render) {
+		var options = this.config;
+		if (this.tabElem) {
+			this.tabElem.remove();
+			this.tabElem = null;
+			// 成功渲染之后的回调
+			render !== true && typeof options.end === 'function' && options.end(this.elem[0]);
+			render === true && this.render();
+		}
+	}
+
+	// 渲染方法供外
+	laydateTab.render = function(options) {
+		var cls = new Class(options);
+		return {
+			config: cls.config
+		};
+	}
+
+	// 加载css
+	laydateTab.ready();
+	//加载方式
+	isLayui ? (
+		layui.define('laydate', function(exports) { //layui加载
+			lay = window.lay;
+			laydate = layui.laydate;
+			laydateTab.path = layui.cache.dir;
+			exports(MOD_NAME, laydateTab);
+		})
+	) : (
+		(typeof define === 'function' && define.amd) ? define(function() { //requirejs加载
+			return laydateTab;
+		}) : function() { //普通script标签加载
+			window.laydateTab = laydateTab;
+		}()
+	);
+
+}();
